34f134103079859f2ebda3725eadf309781e6bbc
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 16:19:58 +0100

    Add translations

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,9 @@
+<?php
+
+use EscolaLms\Tags\Enums\TagsPermissionsEnum;
+
+return [
+    TagsPermissionsEnum::TAGS_CREATE => 'Utwórz tag',
+    TagsPermissionsEnum::TAGS_DELETE => 'Usuń tag',
+    TagsPermissionsEnum::TAGS_UPDATE => 'Aktualizuj tag',
+];
